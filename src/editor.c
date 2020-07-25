8b38c3b31dc20686647055ef9c4c45da572c20e5
lemodelemos <joaolemss@gmail.com>
2020-07-25 02:56:46 -0300

    added home and end keys

--- src/editor.c
+++ src/editor.c
@@ -134,8 +134,12 @@ int editorReadKey() {
 
                 if(seq[2] == '~') {
                     switch(seq[1]) {
+                        case '1': return HOME_KEY;
+                        case '4': return END_KEY;
                         case '5': return PAGE_UP;
                         case '6': return PAGE_DOWN;
+                        case '7': return HOME_KEY;
+                        case '8': return END_KEY;
                     }
                 }
             } else {
@@ -144,7 +148,14 @@ int editorReadKey() {
                     case 'B': return ARROW_DOWN;
                     case 'C': return ARROW_RIGHT;
                     case 'D': return ARROW_LEFT;
+                    case 'H': return HOME_KEY;
+                    case 'F': return END_KEY;
                 }
+            }
+        } else if(seq[0] == '0') {
+            switch(seq[1]) {
+                case 'H': return HOME_KEY;
+                case 'F': return END_KEY;
             }
         }
         return '\x1b';
@@ -161,6 +172,13 @@ void editorProcessKeypress() {
             write(STDOUT_FILENO, "\x1b[2J", 4);
             write(STDOUT_FILENO, "\x1b[H", 3);
             exit(0);
+            break;
+
+        case HOME_KEY:
+            E.cx = 0;
+            break;
+        case END_KEY:
+            E.cx = E.screencols - 1;
             break;
 
         case PAGE_DOWN:
--- src/editor.h
+++ src/editor.h
@@ -11,8 +11,10 @@ enum editorKey {
     ARROW_RIGHT, 
     ARROW_UP, 
     ARROW_DOWN,
+    HOME_KEY,
+    END_KEY,
     PAGE_UP,
-    PAGE_DOWN
+    PAGE_DOWN,
 };
 
 struct editorConfig {
