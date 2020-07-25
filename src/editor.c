1e4f81981dcc0c7885c53f6cc4b4a868c76eabb0
lemodelemos <joaolemss@gmail.com>
2020-07-25 02:58:46 -0300

    added delete key

--- src/editor.c
+++ src/editor.c
@@ -135,6 +135,7 @@ int editorReadKey() {
                 if(seq[2] == '~') {
                     switch(seq[1]) {
                         case '1': return HOME_KEY;
+                        case '3': return DEL_KEY;
                         case '4': return END_KEY;
                         case '5': return PAGE_UP;
                         case '6': return PAGE_DOWN;
--- src/editor.h
+++ src/editor.h
@@ -11,6 +11,7 @@ enum editorKey {
     ARROW_RIGHT, 
     ARROW_UP, 
     ARROW_DOWN,
+    DEL_KEY,
     HOME_KEY,
     END_KEY,
     PAGE_UP,
