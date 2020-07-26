dfd3ab56ae3058cd49fb0a2447436fba98316f0d
lemodelemos <joaolemss@gmail.com>
2020-07-26 18:24:25 -0300

    backspace on the start of a line now append the contents of that line to the previous line

--- src/editor.c
+++ src/editor.c
@@ -434,6 +434,21 @@ void editorAppendRow(char *s, size_t len) {
     E.dirty++;
 }
 
+void editorFreeRow(erow *row) {
+    free(row->render);
+    free(row->chars);
+}
+
+void editorDelRow(int at) {
+    if (at < 0 || at >= E.numrows) return;
+
+    editorFreeRow(&E.row[at]);
+    memmove(&E.row[at], &E.row[at + 1], sizeof(erow) * (E.numrows - at - 1));
+
+    E.numrows--;
+    E.dirty++;
+}
+
 void editorRowInsertChar(erow *row, int at, int c) {
     if(at < 0 || at > row->size)
         at = row->size;
@@ -443,6 +458,17 @@ void editorRowInsertChar(erow *row, int at, int c) {
     row->chars[at] = c;
 
     editorUpdateRow(row);
+    E.dirty++;
+}
+
+void editorRowAppendString(erow *row, char *s, size_t len) {
+    row->chars = realloc(row->chars, row->size + len + 1);
+
+    memcpy(&row->chars[row->size], s, len);
+    row->size += len;
+    row->chars[row->size] = '\0';
+    editorUpdateRow(row);
+
     E.dirty++;
 }
 
@@ -466,11 +492,18 @@ void editorInsertChar(int c) {
 void editorDelChar() {
     if(E.cy == E.numrows)
         return;
+    if(E.cy == 0 && E.cx == 0)
+        return;
 
     erow *row = &E.row[E.cy];
     if(E.cx > 0) {
         editorRowDelChar(row, E.cx - 1);
         E.cx--;
+    } else {
+        E.cx = E.row[E.cy - 1].size;
+        editorRowAppendString(&E.row[E.cy - 1], row->chars, row->size);
+        editorDelRow(E.cy);
+        E.cy--;
     }
 }
 
--- src/editor.h
+++ src/editor.h
@@ -87,7 +87,10 @@ void editorDrawMessageBar(struct abuf *ab);
 int editorRowCxToRx(erow *row, int cx);
 void editorUpdateRow(erow *row);
 void editorAppendRow(char *s, size_t len);
+void editorFreeRow(erow *row);
+void editorDelRow(int at);
 void editorRowInsertChar(erow *row, int at, int c);
+void editorRowAppendString(erow *row, char *s, size_t len);
 void editorRowDelChar(erow *row, int at);
 
 /* Editor operations */
